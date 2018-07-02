a5611868b61908e595b25c5edbfc98c9f1a30449
Ariel Favio Carrizo <arifacarr@gmail.com>
2018-07-02 00:25:14 -0300

    Revert "Completado con la prueba"
    This reverts commit 5148d278e5875ee8e2e6e0eaca4437d4e8ea5a0e.

--- src/ar/edu/uca/TeoriaDeLenguajes/TPEstimacionPolinomicaGenetica/PolynomialGeneticEstimator/Individual.java
+++ src/ar/edu/uca/TeoriaDeLenguajes/TPEstimacionPolinomicaGenetica/PolynomialGeneticEstimator/Individual.java
@@ -1,14 +1,10 @@
 package ar.edu.uca.TeoriaDeLenguajes.TPEstimacionPolinomicaGenetica.PolynomialGeneticEstimator;
 
-import java.util.Random;
 import java.util.function.Function;
-import java.util.stream.IntStream;
 
-public final class Individual {
+final class Individual {
 	private float[] coeficients;
 	private Function<Float, Float> function;
-	
-	
 	
 	/**
 	 * @post Crea un individuo con los coeficientes
@@ -30,72 +26,9 @@ public final class Individual {
 	}
 	
 	/**
-	 * @post Devuelve los coeficientes estimados de la función
-	 */
-	public float[] getCoeficients() {
-		return this.coeficients.clone();
-	}
-	
-	/**
-	 * @pre El otro individuo no puede ser nulo
-	 * @post Realiza una cruza con el individuo especificado,
-	 * 		 devolviendo otro individuo.
-	 */
-	public Individual crossover(Individual other) {
-		if ( other == null ) {
-			throw new NullPointerException();
-		}
-
-		if ( other.coeficients.length != this.coeficients.length ) {
-			throw new IllegalArgumentException("Number of coeficients mismatch");
-		}
-		
-		// Crossover uniforme
-		float[] newCoeficients = new float[this.coeficients.length];
-		
-		for ( int i = 0; i<this.coeficients.length; i++ ) {
-			newCoeficients[i] = Common.getRng().nextBoolean() ? other.coeficients[i] : this.coeficients[i];
-		}
-		
-		return new Individual(newCoeficients);
-	}
-	
-	/**
-	 * @post Realiza una mutación del individuo,
-	 * 		 devolviendo otro.
-	 * 		 Con el delta de variación especificado
-	 */
-	public Individual mutate(float deltaMax) {
-		float[] newCoeficients = new float[this.coeficients.length];
-		
-		for ( int i = 0; i<this.coeficients.length ; i++ ) {
-			float t = Common.getRng().nextFloat();
-			
-			newCoeficients[i] = this.coeficients[i] + deltaMax * (t * 2.0f - 1.0f);
-		}
-		
-		return new Individual(newCoeficients);
-	}
-	
-	/**
 	 * @post Devuelve la función representada por el individuo
 	 */
 	public Function<Float, Float> getEstimatedFunction() {
 		return this.function;
-	}
-	
-	@Override
-	public String toString() {
-		String str = "";
-		
-		for ( int i = this.coeficients.length-1;i>=0; i-- ) {
-			str += this.coeficients[i] + " * x^" + i;
-			
-			if ( i != 0 ) {
-				str += "+ ";
-			}
-		}
-		
-		return str;
 	}
 }
--- src/ar/edu/uca/TeoriaDeLenguajes/TPEstimacionPolinomicaGenetica/PolynomialGeneticEstimator/PolynomialGeneticEstimator.java
+++ src/ar/edu/uca/TeoriaDeLenguajes/TPEstimacionPolinomicaGenetica/PolynomialGeneticEstimator/PolynomialGeneticEstimator.java
@@ -4,7 +4,6 @@ import java.util.function.Function;
 
 import javafx.util.Pair;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -13,26 +12,25 @@ import java.util.List;
 import java.util.Map;
 
 public final class PolynomialGeneticEstimator {
-	private final float[] originalImageValues;
+	private float[] originalImageValues;
 	
-	private final List<Pair<Individual, Float>> poblation;
+	private float minX;
+	private float maxX;
 	
-	private final float minX;
-	private final float maxX;
-	
-	private final EvolutionConfig evolutionConfig;
-	
-	private final int numberOfDomainNumbers;
-	
-	private int iterationNumber;
+	private int numberOfDomainSamples;
 	
 	/**
 	 * @post Crea un estimador con la funci�n objetivo de reales a reales,
-	 * 		 el intervalo de estimaci�n, los coeficientes iniciales, y la configuraci�n de evoluci�n
+	 * 		 el intervalo de estimaci�n, y la cantidad de muestras deseadas
 	 */
-	public PolynomialGeneticEstimator(Function<Float, Float> function, float minX, float maxX, float[] initialCoefficients, EvolutionConfig evolutionConfig) {
-		Common.checkNotNull("function", function);
-		Common.checkNotNull("evolutionConfig", evolutionConfig);
+	public PolynomialGeneticEstimator(Function<Float, Float> function, float minX, float maxX, int numberOfSamples) {
+		if ( function == null ) {
+			throw new NullPointerException();
+		}
+		
+		if ( numberOfSamples <= 0 ) {
+			throw new IllegalArgumentException("Expected positive number of samples");
+		}
 		
 		if ( !(maxX > minX) ) {
 			throw new IllegalArgumentException("Expected maxX > minX");
@@ -40,40 +38,24 @@ public final class PolynomialGeneticEstimator {
 		
 		this.minX = minX;
 		this.maxX = maxX;
-		
-		this.numberOfDomainNumbers = initialCoefficients.length;
-		
-		this.evolutionConfig = evolutionConfig;
+		this.numberOfDomainSamples = numberOfSamples;
 		
 		this.originalImageValues = this.imageSamples(function);
-		
-		Individual referenceIndividual = new Individual(initialCoefficients);
-		this.poblation = new ArrayList<Pair<Individual, Float>>();
-
-		Float deltaMax = this.evolutionConfig.getMutationDeltaByIteration().apply(0);
-		
-		for ( int i=0; i<evolutionConfig.getPoblationSize(); i++ ) {
-			this.poblation.add( individualWithQuadraticError( referenceIndividual.mutate(deltaMax) ) );
-		}
-		
-		this.sortPoblation();
-		
-		this.iterationNumber = 1;
 	}
 	
 	/**
 	 * @post Devuelve el n�mero de muestras de la imagen
 	 * 		 de la funci�n especificada
 	 */
-	private float[] imageSamples(Function<Float, Float> function) {
-		float[] imageSamples = new float[this.numberOfDomainNumbers];
-		
-		for ( int i = 0; i<this.numberOfDomainNumbers; i++ ) {
-			imageSamples[i] = function.apply( this.minX + ( this.maxX - this.minX ) * (float) i / (this.numberOfDomainNumbers-1) );
-		}
-		
-		return imageSamples;
-	}
+    private float[] imageSamples(Function<Float, Float> function) {
+    	float[] imageSamples = new float[this.numberOfDomainSamples];
+    	
+    	for ( int i = 0; i<this.numberOfDomainSamples; i++ ) {
+    		imageSamples[i] = function.apply( this.minX + ( this.maxX - this.minX ) * (float) i / (this.numberOfDomainSamples-1) );
+    	}
+    	
+    	return imageSamples;
+    }
 	
 	/**
 	 * @post Dada los valores estimados de la imagen devuelve el error
@@ -104,79 +86,20 @@ public final class PolynomialGeneticEstimator {
 	}
 	
 	/**
-	 * @post Ordena la poblaci�n por error, ascendente
-	 */
-	private void sortPoblation() {
-		Collections.sort(this.poblation, (individual1, individual2) -> Float.compare(individual1.getValue(), individual2.getValue() ) );
-	}
-	
-	/**
-	 * @post Entre las miembros de la poblaci�n selecciona los m�s aptos,
+	 * @post Entre las miembros de la poblaci�n especificada selecciona los m�s aptos,
 	 * 		 con el n�mero m�ximo de miembros a conservar.
+	 * 		 Devuelve las m�s aptos.
 	 */
-	private void selection() {
-		this.sortPoblation();
+	private Pair<Individual, Float>[] selection(Pair<Individual, Float>[] poblation, int maxNumberOfMembersToConservate) {
+		Pair<Individual, Float>[] orderedIndividualsByAscendingOrder = poblation.clone();
 		
-		while ( poblation.size() > this.evolutionConfig.getPoblationSize() ) {
-			poblation.remove(poblation.size()-1);
-		}
-	}
-	
-	/**
-	 * @post Entre los miembros de la poblaci�n especificada realiza aleatoriamente
-	 * 		 un entrecruzamiento
-	 */
-	private void crossOver() {
-		int originalSize = this.poblation.size();
+		Arrays.sort(orderedIndividualsByAscendingOrder, (individual1, individual2) -> Float.compare(individual1.getValue(), individual2.getValue() ) );
 		
-		for ( int i = 0; i<originalSize; i++ ) {
-			for ( int j = i+1; j<originalSize; j++ ) {
-				if ( Common.getRng().nextFloat() <= this.evolutionConfig.getProbabilityOfCrossOver() ) {
-					this.poblation.add(
-						individualWithQuadraticError(
-							this.poblation.get(i).getKey().crossover(this.poblation.get(j).getKey())
-						)
-					);
-				}
-			}
+		if ( orderedIndividualsByAscendingOrder.length > maxNumberOfMembersToConservate ) {
+			return Arrays.copyOf(orderedIndividualsByAscendingOrder, maxNumberOfMembersToConservate);
 		}
-	}
-	
-	/**
-	 * @post Entre los miembros de la poblaci�n especificada realiza una mutaci�n
-	 */
-	private void mutation() {
-		float deltaMax = this.evolutionConfig.getMutationDeltaByIteration().apply(this.iterationNumber);
-		
-		for ( int i = 0; i<poblation.size(); i++ ) {
-			if ( Common.getRng().nextFloat() <= this.evolutionConfig.getProbabilityOfMutation() ) {
-				this.poblation.set(i, this.individualWithQuadraticError(this.poblation.get(i).getKey().mutate(deltaMax)));
-			}
+		else {
+			return orderedIndividualsByAscendingOrder;
 		}
-	}
-	
-	/**
-	 * @post Realiza un ciclo
-	 */
-	public void doOneCycle() {
-		this.selection();
-		this.crossOver();
-		this.mutation();
-		this.iterationNumber++;
-	}
-	
-	/**
-	 * @post Devuelve el n�mero de iteraci�n
-	 */
-	public int getIterationNumber() {
-		return this.iterationNumber;
-	}
-	
-	/**
-	 * @post Devuelve la soluci�n m�s �ptima por ahora,
-	 * 		 con el error cuadr�tico medio
-	 */
-	public Pair<Individual, Float> getBestIndividual() {
-		return new Pair<Individual, Float>( this.poblation.get(0).getKey(), this.poblation.get(0).getValue() / (float) this.poblation.size() );
 	}
 }
